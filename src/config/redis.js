6bcbb146afe585c66d267ad6d356b2e4f4decd54
Reginaldo <reginaldolribeiro@gmail.com>
2019-02-27 20:40:30 -0300

    Queue

---
+++ src/config/redis.js
@@ -0,0 +1,11 @@
+module.exports = {
+  host: 'localhost',
+  port: 6379
+}
+
+/* module.exports = {
+  host: 'redis-17223.c91.us-east-1-3.ec2.cloud.redislabs.com',
+  pass: 'QybUGcB08UYXPyB4EdPqvc5V3YnPU9zo',
+  port: 17223
+}
+ */
