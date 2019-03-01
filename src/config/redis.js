4bbd3cd546de56e5148fff69afc648383afe5fe0
Reginaldo <reginaldolribeiro@gmail.com>
2019-02-28 21:04:53 -0300

    Exception handling

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,5 +1,5 @@
 module.exports = {
-  host: 'localhost',
+  host: '127.0.0.1',
   port: 6379
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,51 @@
 const express = require('express')
+const validate = require('express-validation')
+const handle = require('express-async-handler')
 
 const routes = express.Router()
 
 const authMiddleware = require('./app/middlewares/auth')
 
 const controllers = require('./app/controllers')
+const validators = require('./app/validators')
 
 /* const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController') */
 
-routes.post('/users', controllers.UserController.store)
-routes.post('/sessions', controllers.SessionController.store)
-routes.get('/users', controllers.UserController.index)
+routes.post(
+  '/users',
+  validate(validators.User),
+  handle(controllers.UserController.store)
+)
+routes.post(
+  '/sessions',
+  validate(validators.Session),
+  handle(controllers.SessionController.store)
+)
+routes.get('/users', handle(controllers.UserController.index))
 
 /* routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true })) */
 
 routes.use(authMiddleware)
 
-routes.get('/ads', controllers.AdController.index)
-routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.update)
-routes.delete('/ads/:id', controllers.AdController.destroy)
+routes.get('/ads', handle(controllers.AdController.index))
+routes.get('/ads/:id', handle(controllers.AdController.show))
+routes.post(
+  '/ads',
+  validate(validators.Ad),
+  handle(controllers.AdController.store)
+)
+routes.put(
+  '/ads/:id',
+  validate(validators.Ad),
+  handle(controllers.AdController.update)
+)
+routes.delete('/ads/:id', handle(controllers.AdController.destroy))
 
-routes.post('/purchase', controllers.PurchaseController.store)
+routes.post(
+  '/purchase',
+  validate(validators.Purchase),
+  handle(controllers.PurchaseController.store)
+)
 
 module.exports = routes
